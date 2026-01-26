5b234ef8065e2dcfbebd9f3f959ba46055165803
abdulfaris.b <abdulfaris.b@digipen.edu>
2026-01-26 12:18:02 +0800

    added coord conversion
    computation now supports conversion from normalised and screen coords to world coords for individual points and Point struct

--- Source/Computation.h
+++ Source/Computation.h
@@ -2,5 +2,10 @@
 #include "MasterHeader.h"
 
 namespace Computation {
+	f32 denormalizePoint(f32& x, f32& y);
+	f32 denormalizePoint(Point &x);
+	f32 screenToWorld(f32& x, f32& y);
+	f32 screenToWorld(Point & x);
 	f32 calcSquaredDistance(Point a, Point b);
+	//f32 collisionAABB(Point a, Point b);
 }
