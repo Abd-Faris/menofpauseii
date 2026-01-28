f9def2252af0a2c7398a8593366861cf8e425f68
abdulfaris.b <abdulfaris.b@digipen.edu>
2026-01-28 18:02:11 +0800

    Update Structs.h
    added Card struct and enums

--- Source/Structs.h
+++ Source/Structs.h
@@ -1,15 +1,50 @@
 #pragma once
 #include "AEEngine.h"
 
+enum Rarity {
+	COMMON = 0,
+	RARE,
+	EPIC,
+	UNIQUE,
+	NUM_OF_RARITIES
+};
+
+enum Type {
+	HP = 0,
+	DMG,
+	FIRE_RATE,
+	MOVE_SPEED,
+	XP,
+	NUM_OF_TYPES
+};
+
 struct gfxtext {
 	const char* text;
 	f32 x, y, scale, r, g, b, a;
 };
 
-// OLD: USE native AEVec2 instead
-//struct Point {
-//	f32 x, y;
-//};
+struct Card {
+	bool exists = false;
+	AEGfxVertexList* mesh{};
+	int type{}, val{}; // card stats
+	f32 xpos{}, ypos{}; // card position (world coords)
+	f32 sizex{ 250 }, sizey{ 350 }; // card size
+	// lowest priority for implementation
+	int rarity{ Rarity::COMMON };
+
+	// generates new card and assigns to 
+	void generateCard() {
+		// randomise type
+		type = static_cast<int>(AERandFloat() * Type::NUM_OF_TYPES);
+		// alter percentage based on rarity
+		switch (rarity) {
+		case Rarity::COMMON: // 10-30%, range 20%
+			val = 10 + static_cast<int>(AERandFloat() * 20);
+			break;
+		default: break;
+		};
+	}
+};
 
 struct shape {
 	f32 scale;			//scale
