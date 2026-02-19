420049091123d41f61de647ae332c67d342f63a3
Wong Shao Wei <wongswei@gmail.com>
2026-02-19 18:00:27 +0800

    nothing burger

--- Source/Debug3.cpp
+++ Source/Debug3.cpp
@@ -13,6 +13,165 @@ namespace {
 	AEGfxVertexList* MeshRect{};
 	
 }
+bool CollisionIntersection_RectRect_Dynamic(const AABB& aabb1,				//Input
+	const AEVec2& vel1,				//Input 
+	const AABB& aabb2,				//Input 
+	const AEVec2& vel2,				//Input
+	float& firstTimeOfCollision)	//Output: the calculated value of tFirst
+{
+	/*
+	Implement the dynamic collision intersection over here.
+
+	The steps "from the lecture slides" are:
+
+	Step 2: Initialize and calculate the new "relative" velocity of Vb
+			tFirst = 0  //tFirst variable is commonly used for both the x-axis and y-axis
+			tLast = dt  //tLast variable is commonly used for both the x-axis and y-axis
+
+	Step 3: Working with one dimension (x-axis).
+			if(Vb < 0)
+				case 1
+				case 4
+			else if(Vb > 0)
+				case 2
+				case 3
+			else //(Vb == 0)
+				case 5
+
+			case 6
+
+	Step 4: Repeat step 3 on the y-axis
+
+	Step 5: Return true: the rectangles intersect
+	*/
+
+	float tFirst = 0.0f; // Time of start of frame
+	float tLast = 0.0f; // Time of end of frame
+	// Step 2: Calculate relative velocity (B relative to A)
+	AEVec2 vRel{}; // Vector for relative velocity of both objects
+
+
+	vRel.x = vel2.x - vel1.x; // vRel = velocity of 2nd object - velocity of first object
+	vRel.y = vel2.y - vel1.y;
+
+	// Initialize time values
+	tFirst = 0.0f;
+	tLast = (float)dt;
+
+	// Step 3: Working with one dimension (X-axis)
+	if (vRel.x < 0.0f) // if relative velocity is below 0
+	{
+		// Case 1: B moving left, check if already separated and moving away
+		if (aabb1.min.x > aabb2.max.x)
+			return false;
+		// Case 4: Calculate tFirst and tLast
+		if (aabb1.max.x < aabb2.min.x) {
+			float tFirstX = (aabb1.max.x - aabb2.min.x) / vRel.x;
+			if (tFirstX > tFirst) { // Find if earliest time taken for collision is more than the first frame
+				tFirst = tFirstX;
+			}
+		}
+
+		if (aabb1.min.x < aabb2.max.x) {
+			float tLastX = (aabb1.min.x - aabb2.max.x) / vRel.x;
+			if (tLastX < tLast) { // Find if earliest time taken for collision to exit is less than the last frame
+				tLast = tLastX;
+			}
+		}
+	}
+	else if (vRel.x > 0.0f)
+	{
+		// Case 3: B moving right, check if already separated and moving away
+		if (aabb1.max.x < aabb2.min.x)
+			return false;
+
+		// Case 2: Calculate tFirst and tLast
+		if (aabb2.max.x < aabb1.min.x) {
+			float tFirstX = (aabb1.min.x - aabb2.max.x) / vRel.x;
+			if (tFirstX > tFirst) { // Find if earliest time taken for collision is more than the first frame
+				tFirst = tFirstX;
+			}
+		}
+
+		if (aabb2.min.x < aabb1.max.x) {
+			float tLastX = (aabb1.max.x - aabb2.min.x) / vRel.x;
+			if (tLastX < tLast) { // Find if earliest time taken for collision to exit is less than the last frame
+				tLast = tLastX;
+			}
+		}
+	}
+	else // vRel.x == 0
+	{
+		// Case 5: Check if separated on X
+		if (aabb1.max.x < aabb2.min.x)
+			return false;
+		if (aabb1.min.x > aabb2.max.x)
+			return false;
+	}
+
+	// Case 6: Check if time window collapsed
+	if (tFirst > tLast)
+		return false;
+
+	// Step 4: Repeat on Y-axis
+	if (vRel.y < 0.0f)
+	{
+		// Case 1: B moving down, check if already separated and moving away
+		if (aabb1.min.y > aabb2.max.y)
+			return false;
+
+		if (aabb1.max.y < aabb2.min.y) {
+			float tFirstY = (aabb1.max.y - aabb2.min.y) / vRel.y;
+			if (tFirstY > tFirst) { // Find if earliest time taken for collision to exit is more than the first frame
+				tFirst = tFirstY;
+			}
+		}
+
+		if (aabb1.min.y < aabb2.max.y) {
+			float tLastY = (aabb1.min.y - aabb2.max.y) / vRel.y;
+			if (tLastY < tLast) { // Find if earliest time taken for collision to exit is less than the last frame
+				tLast = tLastY;
+			}
+		}
+	}
+	else if (vRel.y > 0.0f)
+	{
+		// Case 3: B moving up, check if already separated and moving away
+		if (aabb1.max.y < aabb2.min.y)
+			return false;
+		if (aabb2.max.y < aabb1.min.y) {
+			float tFirstY = (aabb1.min.y - aabb2.max.y) / vRel.y;
+			if (tFirstY > tFirst) { // Find if earliest time taken for collision to exit is more than the first frame
+				tFirst = tFirstY;
+			}
+		}
+
+		if (aabb2.min.y < aabb1.max.y) {
+			float tLastY = (aabb1.max.y - aabb2.min.y) / vRel.y;
+			if (tLastY < tLast) { // Find if earliest time taken for collision to exit is less than the last frame
+				tLast = tLastY;
+			}
+		}
+	}
+	else // vRel.y == 0
+	{
+		// Case 5: Check if separated on Y
+		if (aabb1.max.y < aabb2.min.y)
+			return false;
+		if (aabb1.min.y > aabb2.max.y)
+			return false;
+	}
+
+	// Case 6: Check if time window collapsed
+	if (tFirst > tLast)
+		return false;
+
+	firstTimeOfCollision = tFirst; // Update the time for first collision outside the function
+	return true; // Collided if all previous condition is checked
+
+
+
+}
 
 void DrawEnemyMesh() {
 
@@ -225,6 +384,31 @@ void SpawnEnemies(Enemies* enemy) {
 }
 
 void SpawnBigEnemies(Enemies* enemy) {
+
+	float setscale = 67;
+
+	float posx = static_cast<float>((AERandFloat() * AEGfxGetWindowWidth()) - AEGfxGetWindowWidth() / 2) - setscale;
+	if (posx < (-AEGfxGetWindowWidth() / 2) + setscale) {
+		posx = (-AEGfxGetWindowWidth() / 2) + setscale;
+	}
+
+	float posy = static_cast<float>((AERandFloat() * AEGfxGetWindowHeight()) - AEGfxGetWindowHeight() / 2) - setscale;
+	if (posy < (-AEGfxGetWindowHeight() / 2) + setscale) {
+		posy = (-AEGfxGetWindowHeight() / 2) + setscale;
+	}
+
+	enemy->pos.x = posx;
+	enemy->pos.y = posy;
+	enemy->scale = setscale;
+	enemy->xp = 25;
+	enemy->rotation = static_cast<float>(AERandFloat() * 360);
+	enemy->alive = true;
+	enemy->hp = 100;
+	enemy->velocity.x = 0;
+	enemy->velocity.y = 0;
+}
+
+void SpawnKEnemies(Enemies* enemy) {
 
 	float setscale = 67;
 
