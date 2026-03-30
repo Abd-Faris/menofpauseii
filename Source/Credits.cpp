a786e27d6dd0c62556af9a981fba15b1dc274886
Wong Shao Wei <wongswei@gmail.com>
2026-03-30 12:33:27 +0800

    Credits work yippee

--- Source/Credits.cpp
+++ Source/Credits.cpp
@@ -6,42 +6,52 @@
 // ------------------------------------------------------------------------- //
 
 #include "MasterHeader.h"
-#include "credits.h"
-
 
 std::vector<GfxText> CreditTexts{
 		{"Men of Pause Production",    1.f, 0, 0, 0, 255, {0, -200}},
-		{"Digipen Executives\nPresident Claude Comair\nPrasanna Ghali", 1.f, 0, 0, 0, 255, {0, 0}},
-		{"Instructors\nGerald\nTommy\nDr. Sooroor\n",    1.f, 0, 0, 0, 255, {0, 200}},
+		{"Digipen Executives\n\nPresident Claude Comair\n\nPrasanna Ghali", 1.f, 0, 0, 0, 255, {0, 0}},
+		{"Instructors\n\nGerald\n\nTommy\n\nDr. Sooroor\n",    1.f, 0, 0, 0, 255, {0, 200}},
 		{"Our Team\nFaris\nIzzat the goat\nShao Wei\nZi Hao\n",    1.f, 0, 0, 0, 255, {0, 400}},
-		{"Assets\nGraphics\nPiskel\nFontMeme\nKenney\n",    1.f, 0, 0, 0, 255, {0, 600}},
+		{"Assets\n\nGraphics\nPiskel\nFontMeme\nKenney\n",    1.f, 0, 0, 0, 255, {0, 600}},
 		{"Audio\nBosca Ceoil\nSoundly\n",    1.f, 0, 0, 0, 255, {0, 800}},
 };
 
 void LoadCredits() {
+	
+
+}
+void InitializeCredits() {
 	struct AEVec2 init[] = {
-		{ 0, -200 },  // Men of Pause
-		{ 0, 0 }, // Executives
-		{ 0, 200 },
-		{ 0, 400 },
-		{ 0, 600 }, // Instructors
-		{ 0, 800 },
+		{ 0, 0 },  // Men of Pause
+		// Executives
+		{ 0, -400 },
+		{ 0, -800 },
+		{ 0, -1200 }, // Instructors
+		{ 0, -1600 },
+		{ 0, -2000 },
 
 	};
 	for (int i = 0; i < CreditTexts.size(); i++) {
 		CreditTexts[i].pos = init[i];
 	}
-
-}
-void InitializeCredits() {
-
 }
 void UpdateCredits() {
-	while (CreditTexts[5].pos.y > -1000) {
-		for (auto c : CreditTexts) c.pos.y -= 10;
-	}
+	float deltaTime = (float)AEFrameRateControllerGetFrameTime();
+
+	// Scroll up each frame
+	for (auto& c : CreditTexts) // reference not copy
+		c.pos.y += 200.f * deltaTime;
+
+	// Go back to main menu when done
+	if (CreditTexts[5].pos.y > 800.f)
+		GS_next = GS_MAIN_MENU;
+
+	// Or press any key to skip
+	if (AEInputCheckTriggered(AEVK_ESCAPE))
+		GS_next = GS_MAIN_MENU;
 }
 void DrawCredits() {
+	AEGfxSetBackgroundColor(0.2f, 0.2f, 0.2f);
 	for (auto c : CreditTexts)
 	Gfx::printMultiline(c, boldPixels);
 
