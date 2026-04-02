412f9a205f7bf242d1523fe1cf8b6f800ae5f281
Bliz3d <zihaooon3@gmail.com>
2026-04-02 15:06:26 +0800

    another push

--- Source/Input.cpp
+++
@@ -1,9 +0,0 @@
-#include "MasterHeader.h"
-
-// Handles game state changing
-void Debug_States() {
-	if (AEInputCheckCurr(AEVK_0)) GS_next = GS_MAIN_MENU;
-	else if (AEInputCheckCurr(AEVK_1)) GS_next = DEBUG1;
-	else if (AEInputCheckCurr(AEVK_4)) GS_next = GS_CARD_SHOP;
-	else if (AEInputCheckCurr(AEVK_6)) GS_next = GS_GAME;
-}
