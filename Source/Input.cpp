c07cae45943498613a705cee19d924f785b95fbc
Bliz3d <zihaooon3@gmail.com>
2026-04-02 15:04:33 +0800

    tahnk god our game still works

--- Source/Input.cpp
+++ Source/Input.cpp
@@ -4,9 +4,6 @@
 void Debug_States() {
 	if (AEInputCheckCurr(AEVK_0)) GS_next = GS_MAIN_MENU;
 	else if (AEInputCheckCurr(AEVK_1)) GS_next = DEBUG1;
-	else if (AEInputCheckCurr(AEVK_2)) GS_next = DEBUG2;
-	else if (AEInputCheckCurr(AEVK_3)) GS_next = DEBUG3;
 	else if (AEInputCheckCurr(AEVK_4)) GS_next = GS_CARD_SHOP;
-	else if (AEInputCheckCurr(AEVK_5)) GS_next = DEBUG5;
 	else if (AEInputCheckCurr(AEVK_6)) GS_next = GS_GAME;
 }
